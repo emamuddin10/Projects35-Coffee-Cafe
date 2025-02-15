162222ec298886b15f8bb8790c39631591c6f1a7
Emam Uddin <emamskingdom@gmail.com>
2025-02-16 01:51:14 +0600

    added header compo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,27 @@
-import logo from '../assets/logo.png'
+import moment from "moment/moment";
+import logo from "../assets/logo.png";
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
+
 const Header = () => {
-    return (
-        <div className='text-center'>
-            <div className="logo">
-                <img className='mx-auto' src={logo} alt="" />
-            </div>
-            <p>Journalism Without Fear or Favour</p>
-        </div>
-    );
+  return (
+    <div className="text-center space-y-3">
+      <div className="logo mt-20">
+        <img className="mx-auto" src={logo} alt="" />
+      </div>
+      <p className="font-display">Journalism Without Fear or Favour</p>
+      <p>{moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}</p>
+      <div className="flex items-center gap-2 p-2 bg-slate-200 w-11/12 mx-auto ">
+        <span className="bg-red-400 px-3 py-1 text-white">Latest</span>
+        <Marquee speed={20} pauseOnHover={true}>
+          <Link to='/latest'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, id.</Link>
+          <Link to='/latest'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, id.</Link>
+          <Link to='/latest'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, id.</Link>
+        </Marquee>
+      </div>
+      
+    </div>
+  );
 };
 
 export default Header;
