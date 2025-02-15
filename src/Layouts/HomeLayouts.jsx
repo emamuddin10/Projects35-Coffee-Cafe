b05f75e5960569269c636715e50ffa1c438a814b
Emam Uddin <emamskingdom@gmail.com>
2025-02-16 01:52:22 +0600

    added layout

--- src/Layouts/HomeLayouts.jsx
+++ src/Layouts/HomeLayouts.jsx
@@ -1,12 +1,24 @@
 import Header from "../components/Header";
+import LeftAside from "../components/MainLayouts/LeftAside";
+import Navbar from "../components/Navbar";
 
 
 const HomeLayouts = () => {
     return (
-        <div>
-            <header> <Header></Header></header>
-            <nav></nav>
-            <main></main>
+        <div >
+            <header>
+                 <Header></Header>
+            </header>
+            <nav className="w-11/12 mx-auto">
+                <Navbar></Navbar>
+            </nav>
+            <main className="w-11/12 mx-auto grid md:grid-cols-12 gap3">
+                <aside className="col-span-3">
+                    <LeftAside></LeftAside>
+                </aside>
+                <section className="col-span-6">Main Layout</section>
+                <section className="col-span-3">Right Navbar </section>
+            </main>
         </div>
     );
 };
