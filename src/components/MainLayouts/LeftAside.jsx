322adc822d212efa2f5398d86023e8b4b68e8eff
Emam Uddin <emamskingdom@gmail.com>
2025-02-16 23:48:59 +0600

    added social

--- src/components/MainLayouts/LeftAside.jsx
+++ src/components/MainLayouts/LeftAside.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 
 
 const LeftAside = () => {
+    const [categories,setCategories]=useState([])
+    useEffect(()=>{
+        fetch('https://openapi.programming-hero.com/api/news/categories')
+        .then(res=>res.json())
+        .then(data=>setCategories(data.data.news_category))
+    },[])
     return (
         <div>
-            <h1 className="font-semibold">All Category</h1>
+            <h1 className="font-semibold">All Category ({categories.length})</h1>
+            <div className="flex flex-col gap-3 py-5">
+                {
+                    categories.map(category=> <button className="btn " key={category.category_id}>{category.category_name}</button>)
+                }
+            </div>
         </div>
     );
 };
