28e49507e91273b30f314e2a2766872dc2177046
Emam Uddin <emamskingdom@gmail.com>
2025-02-17 03:02:40 +0600

    added social login

---
+++ src/components/SocialLogin.jsx
@@ -0,0 +1,16 @@
+import { FaGoogle } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+const SocialLogin = () => {
+    return (
+        <div>
+            <h1 className="font-semibold">Login With</h1>
+            <div className="*:w-full space-y-3">
+                <button className='text-blue-400 btn '><FaGoogle /> Login With Google</button>
+                <button className="btn "> <FaGithub /> Login With Github</button>
+            </div>
+        </div>
+    );
+};
+
+export default SocialLogin;
