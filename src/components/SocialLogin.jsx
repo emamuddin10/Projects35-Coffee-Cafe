48bb2f1e2719d1cb3a1420ebc2ac5267cde4e24d
Emam Uddin <emamskingdom@gmail.com>
2025-02-17 22:49:24 +0600

    added inside

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa";
 const SocialLogin = () => {
     return (
         <div>
-            <h1 className="font-semibold">Login With</h1>
+            <h1 className="font-semibold">Login With in</h1>
             <div className="*:w-full space-y-3">
                 <button className='text-blue-400 btn '><FaGoogle /> Login With Google</button>
                 <button className="btn "> <FaGithub /> Login With Github</button>
