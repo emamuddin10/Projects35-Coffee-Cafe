31403e7ef97875435a62026e672918d127c0a7ab
Emam Uddin <emamskingdom@gmail.com>
2025-02-20 17:55:23 +0600

    added auth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 import user from '../assets/user.png'
 const Navbar = () => {
     return (
-        <div className="flex justify-between items-center py-5">
+        <div className="flex justify-between items-center py-5"> 
             <div></div>
             <div className="flex gap-3">
-                <Link to='/'>Home</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/career'>Career</Link>
+                <Link className="font-semibold" to='/'>Home</Link>
+                <Link className="font-semibold" to='/about'>About</Link>
+                <Link className="font-semibold" to='/career'>Career</Link>
             </div>
             <div className="flex gap-3 items-center">
                 <img src={user} alt="" />
-                <button className="btn btn-neutral rounded-none px-5">Login</button>
+                <Link to='/auth/login' className="btn btn-neutral rounded-none px-5">Login</Link>
 
             </div>
         </div>
