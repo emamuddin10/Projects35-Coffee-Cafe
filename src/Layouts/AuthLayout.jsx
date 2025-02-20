81eadb749f76a78f51a48ac3fc02ab21d36cf9e2
Emam Uddin <emamskingdom@gmail.com>
2025-02-20 17:56:03 +0600

    added auth layout

---
+++ src/Layouts/AuthLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
+const AuthLayout = () => {
+    return (
+        <div className='w-11/12 mx-auto '>
+            <header>
+                <Navbar></Navbar>
+            </header>
+            <Outlet>  </Outlet>
+        </div>
+    );
+};
+
+export default AuthLayout;
