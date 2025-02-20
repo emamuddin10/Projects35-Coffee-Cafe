b7599b9def9971e80779d762ee89127f9347cc85
Emam Uddin <emamskingdom@gmail.com>
2025-02-20 17:56:22 +0600

    added login

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="flex justify-center items-center ">
+      <div className="card bg-base-100 w-full max-w-lg shrink-0 shadow-2xl p-10">
+        <h1 className="font-semibold text-3xl text-center text-slate-400">Login Your Account</h1>
+        <div className="card-body">
+          <fieldset className="fieldset">
+            <label className="fieldset-label">Email</label>
+            <input type="email" className="input" placeholder="Enter Your Email Address" />
+            <label className="fieldset-label">Password</label>
+            <input type="password" className="input" placeholder=" Enter Your Password" />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-neutral mt-4">Login</button>
+          </fieldset>
+          <p className="text-center">Don't have an Account ? <Link className='text-red-400 font-semibold text-center' to='/auth/register'>Register</Link></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
