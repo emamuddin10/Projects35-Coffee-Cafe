ff2ac96b5d66aaea4811ede50e1aee319f493bbd
Emam Uddin <emamskingdom@gmail.com>
2025-02-20 18:03:14 +0600

    added login

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,39 +1,50 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import HomeLayouts from "../Layouts/HomeLayouts";
 import CategoryNews from "../Pages/CategoryNews";
+import AuthLayout from "../Layouts/AuthLayout";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
 
-
-
-const router =createBrowserRouter([
-    {
-        path:'/',
-        element:<HomeLayouts></HomeLayouts>,
-        children:[
-            {
-               path:'',
-               element:<Navigate to={'/category/01'}></Navigate>
-            },
-            {
-                path:'/category/:id',
-                element:<CategoryNews></CategoryNews> ,
-                loader: ({params})=>
-                    fetch(`https://openapi.programming-hero.com/api/news/category/${params.id}`)
-                
-
-            }
-        ]
-    },
-    {
-        path:'/news',
-        element:<h1>This News</h1>
-    },
-    {
-        path:'/auth',
-        element:<h1>This is Auth Login</h1>
-    },
-    {
-        path:'*',
-        element:<h1>This is Error</h1>
-    }
-])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayouts></HomeLayouts>,
+    children: [
+      {
+        path: "",
+        element: <Navigate to={"/category/01"}></Navigate>,
+      },
+      {
+        path: "/category/:id",
+        element: <CategoryNews></CategoryNews>,
+        loader: ({ params }) =>
+          fetch(
+            `https://openapi.programming-hero.com/api/news/category/${params.id}`
+          ),
+      },
+    ],
+  },
+  {
+    path: "/news",
+    element: <h1>News layout</h1>,
+  },
+  {
+    path: "/auth",
+    element: <AuthLayout></AuthLayout>,
+    children: [
+      {
+        path: "/auth/login",
+        element: <Login></Login>,
+      },
+      {
+        path:'/auth/register',
+        element:<Register></Register>
+      }
+    ],
+  },
+  {
+    path: "*",
+    element: <h1>This is Error</h1>,
+  },
+]);
 export default router;
