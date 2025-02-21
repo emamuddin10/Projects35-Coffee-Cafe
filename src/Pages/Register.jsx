1d02c47b511701263a27869c4901daee99ba86e0
Emam Uddin <emamskingdom@gmail.com>
2025-02-21 22:16:22 +0600

    create register functionality

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,35 +1,65 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../Provider/AuthProvider";
 
 const Register = () => {
+
+  const {createNewUser,setUser}= useContext(AuthContext)
+
+  const handleOnSubmit = (e)=>{
+    e.preventDefault()
+    // get from data value
+    const form = new FormData(e.target)
+    const name = form.get("name")
+    const photo = form.get("photo")
+    const email = form.get("email")
+    const password = form.get("password")
+    console.log({email,name,photo,password})
+    createNewUser(email,password)
+    .then((result)=>{
+      const user = result.user
+      setUser(user)
+      console.log(user)
+    })
+    .catch((error)=>{
+     const  codeError= error.code
+     const  errorMessage= error.Message
+     console.log(codeError,errorMessage)
+    })
+
+  }
   return (
     <div className="flex justify-center items-center ">
       <div className="card bg-base-100 w-full max-w-lg shrink-0 shadow-2xl p-10">
         <h1 className="font-semibold text-3xl text-center text-slate-400">
           Reister Your Account
         </h1>
-        <div className="card-body">
+        <form onSubmit={handleOnSubmit} className="card-body">
           <fieldset className="fieldset">
           <label className="fieldset-label">Name</label>
             <input
+             name="name"
               type="text"
               className="input"
               placeholder="Enter Your Name"
             />
             <label className="fieldset-label">Photo</label>
             <input
+            name="photo"
               type="text"
               className="input"
               placeholder="Enter Your  Photo Url"
             />
             <label className="fieldset-label">Email</label>
             <input
+            name="email"
               type="email"
               className="input"
               placeholder="Enter Your Email"
             />
             <label className="fieldset-label">Password</label>
             <input
+              name="password"
               type="password"
               className="input"
               placeholder=" Enter Your Password"
@@ -48,7 +78,7 @@ const Register = () => {
               Login
             </Link>
           </p>
-        </div>
+        </form>
       </div>
     </div>
   );
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,21 +1,45 @@
-import { useState } from "react"
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import app from "../firebase/firebase.init";
 
-  const AuthContext = createContext()
+export const AuthContext = createContext();
+const auth = getAuth(app);
 
-const AuthProvider = () => {
-    const [user, setUser]=useState(
-        ({
-            name:'jhanker mahbub',
-            email:'jhanker@hungker.com'
-          })
-    )
+const createNewUser = (email,password)=>{
+  return createUserWithEmailAndPassword(auth, email, password)
 
-    const authInfo ={
-        user,
-        setUser
-    }
-
-    return 
-    <AuthContext.Provider value={authInfo}></AuthContext.Provider>
 }
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  console.log(user)
+
+const logOut = ()=>{
+  return signOut(auth)
+}
+const logIn = (email,password)=>{
+  return signInWithEmailAndPassword(auth, email, password)
+}
+
+  const authInfo = {
+    user,
+    setUser,
+    createNewUser,
+    logOut,
+    logIn
+
+  };
+
+  useEffect(()=>{
+   const unSubscribe= onAuthStateChanged(auth,(currentUser)=>{
+      setUser(currentUser)
+    })
+    return ()=>{
+        unSubscribe()
+    }
+  },[])
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
 export default AuthProvider;
