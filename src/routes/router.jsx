4d826caad1c5272ceeaa0f97447ba03cc0ab0045
Emam Uddin <emamskingdom@gmail.com>
2025-02-22 18:17:25 +0600

    added router

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -4,6 +4,9 @@ import CategoryNews from "../Pages/CategoryNews";
 import AuthLayout from "../Layouts/AuthLayout";
 import Login from "../Pages/Login";
 import Register from "../Pages/Register";
+import NewsDetails from "../Pages/NewsDetails";
+import PrivetRoute from "./PrivetRoute";
+
 
 const router = createBrowserRouter([
   {
@@ -25,8 +28,9 @@ const router = createBrowserRouter([
     ],
   },
   {
-    path: "/news",
-    element: <h1>News layout</h1>,
+    path: "/news/:id",
+    element:<PrivetRoute> <NewsDetails></NewsDetails> </PrivetRoute>,
+    loader:({params})=>fetch(`https://openapi.programming-hero.com/api/news/${params.id}`)
   },
   {
     path: "/auth",
